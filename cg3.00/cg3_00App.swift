b65ce45adfe5459a9596a405c6c21ae2ad6e1457
yaroshovychsofi <sofia.yaroshovych@gmail.com>
2024-03-26 11:51:41 +0200

    Initial Commit

---
+++ cg3.00/cg3_00App.swift
@@ -0,0 +1,17 @@
+//
+//  cg3_00App.swift
+//  cg3.00
+//
+//  Created by  Sofia Yaroshovych on 26.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct cg3_00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
