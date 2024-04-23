4467f332b221b58c5b0e4f313c36cb44196521e2
yaroshovychsofi <sofia.yaroshovych@gmail.com>
2024-04-23 20:27:55 +0300

    PhotoPicker

---
+++ cg3.00/ImageConverter.swift
@@ -0,0 +1,8 @@
+//
+//  ImageConverter.swift
+//  cg3.00
+//
+//  Created by  Sofia Yaroshovych on 23.04.2024.
+//
+
+import Foundation
---
+++ cg3.00/PhotoPicker.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoPicker.swift
+//  cg3.00
+//
+//  Created by  Sofia Yaroshovych on 23.04.2024.
+//
+
+import SwiftUI
+
+struct PhotoPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoPicker()
+}
